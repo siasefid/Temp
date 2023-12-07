43896593662634c9861b40ca9ece0463904a6445
amireza <amirreza.sve@gmail.com>
2023-12-07 12:51:27 +0100

    IoT 2023 project

---
+++ app/src/main/java/com/example/temperature/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.temperature;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.Manifest;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private EditText ed;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ed =  findViewById(R.id.text);
+        imageView = findViewById(R.id.imageView);
+        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.RECORD_AUDIO)!=
+                PackageManager.PERMISSION_GRANTED){
+
+        }
+
+    }
+}
