2f7ff7beb221dbfa634f7f5bfc6899ed5ab2f6ea
jacobohlsson <jacob@LAPTOP-27L1FFRR>
2023-12-11 17:25:16 +0100

    Implemented voice recognition, changed the design of the app (added a circle around mic, changed colors, sizes etc.)

--- app/src/main/java/com/example/temperature/MainActivity.java
+++ app/src/main/java/com/example/temperature/MainActivity.java
@@ -3,30 +3,74 @@ package com.example.temperature;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.Manifest;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int SPEECH_REQUEST_CODE = 100 ;
+    private static final int ROOM_2 = 20;
+    private static final int ROOM_3 = 23;
     private Button btn;
-    private EditText ed;
-    private ImageView imageView;
+    private EditText text;
+    private ImageView mic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ed =  findViewById(R.id.text);
-        imageView = findViewById(R.id.imageView);
+        text =  findViewById(R.id.text);
+        mic = findViewById(R.id.imageView);
         if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.RECORD_AUDIO)!=
                 PackageManager.PERMISSION_GRANTED){
-
         }
+        mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSpeechRecognition("Speak");
+            }
+        });
 
+    }
+
+    private void startSpeechRecognition(String prompt) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, prompt);
+
+        try {
+            startActivityForResult(intent, SPEECH_REQUEST_CODE);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "Speech recognition not supported on this device", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        String spokenText = results.get(0);
+
+        if (spokenText.toLowerCase().indexOf("room 2") !=-1) {
+            text.setText("Room 2 temperature is: " + ROOM_2 + "°C");
+        }
+        else if (spokenText.toLowerCase().indexOf("room 3") !=-1) {
+            text.setText("Room 3 temperature is: " + ROOM_3 + "°C");
+        }
+        else {
+            text.setText("command not found");
+        }
     }
 }
