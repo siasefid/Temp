66a6f6a7e28c2b66db05c1cd653655653639941f
siasefid <amirreza.sve@gmail.com>
2023-12-13 13:40:58 +0100

    IoT 2023 project

--- app/src/main/java/com/example/temperature/MainActivity.java
+++ app/src/main/java/com/example/temperature/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.temperature;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.util.Log;
@@ -15,6 +18,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.Manifest;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -23,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int SPEECH_REQUEST_CODE = 100 ;
     private static final int ROOM_2 = 20;
     private static final int ROOM_3 = 23;
-    private Button btn;
+
+    private ToggleButton btn;
     private EditText text;
     private ImageView mic;
 
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         text =  findViewById(R.id.text);
         mic = findViewById(R.id.imageView);
+        btn = findViewById(R.id.toggle);
         if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.RECORD_AUDIO)!=
                 PackageManager.PERMISSION_GRANTED){
         }
@@ -40,16 +46,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startSpeechRecognition("Speak");
+                btn.setChecked(true);
+                btn.setBackgroundColor(Color.GREEN);
             }
         });
 
     }
+
 
     private void startSpeechRecognition(String prompt) {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, prompt);
+
 
         try {
             startActivityForResult(intent, SPEECH_REQUEST_CODE);
@@ -72,5 +82,7 @@ public class MainActivity extends AppCompatActivity {
         else {
             text.setText("command not found");
         }
+        btn.setChecked(false);
+        btn.setBackgroundColor(Color.RED);
     }
 }
