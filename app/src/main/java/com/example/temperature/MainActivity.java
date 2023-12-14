c5f61c2669d2395fd4671f0ccce44de81dd109f4
siasefid <amirreza.sve@gmail.com>
2023-12-14 14:40:53 +0100

    IoT 2023 project

--- app/src/main/java/com/example/temperature/MainActivity.java
+++ app/src/main/java/com/example/temperature/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.temperature;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,9 +9,9 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.Manifest;
@@ -23,14 +21,16 @@ import android.widget.ToggleButton;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     private static final int SPEECH_REQUEST_CODE = 100 ;
     private static final int ROOM_2 = 20;
     private static final int ROOM_3 = 23;
-
     private ToggleButton btn;
     private EditText text;
     private ImageView mic;
+    private TextToSpeech toSpeech;
+    private ArrayList<String> results;
+    private String spokenText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         text =  findViewById(R.id.text);
         mic = findViewById(R.id.imageView);
         btn = findViewById(R.id.toggle);
+        toSpeech = new TextToSpeech(this, this);
         if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.RECORD_AUDIO)!=
                 PackageManager.PERMISSION_GRANTED){
         }
@@ -60,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, prompt);
 
-
         try {
             startActivityForResult(intent, SPEECH_REQUEST_CODE);
         } catch (ActivityNotFoundException e) {
@@ -70,8 +70,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-        String spokenText = results.get(0);
+         results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        spokenText = results.get(0);
 
         if (spokenText.toLowerCase().indexOf("room 2") !=-1) {
             text.setText("Room 2's temperature is: " + ROOM_2 + "°C");
@@ -88,7 +88,28 @@ public class MainActivity extends AppCompatActivity {
         else {
             text.setText("command not found");
         }
+        textToSpeech(text.getText().toString());
         btn.setChecked(false);
         btn.setBackgroundColor(Color.RED);
+    }
+
+    @Override
+    public void onInit(int i) {
+        if(i == TextToSpeech.SUCCESS){
+            int b = toSpeech.setLanguage(Locale.US);
+            if(b == TextToSpeech.LANG_MISSING_DATA || b == TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("TextToSpeech","Language is not supported");
+            }else{
+                Log.e("TextToSpeech", "failed");
+
+            }
+        }
+    }
+    private void textToSpeech(String t) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            toSpeech.speak(t, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            toSpeech.speak(t, TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 }
